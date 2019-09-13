d6898dc15527fbfedcc26146f6fb18043038c367
d0p3t <d0p3t@users.noreply.github.com>
2019-09-13 12:00:44 +0200

    Better screenshot logging

--- sv_ss.lua
+++ sv_ss.lua
@@ -1,22 +1,44 @@
-AddEventHandler("ggcommon:ss", function(player)
-    if(player == -1 or player == nil) then CancelEvent() return end
-
-    local identifiers = GetPlayerIdentifiers(player)
-    local license = 0
-
-    for k,v in pairs(identifiers) do
-        if string.sub(v, 1, string.len("license:")) == "license:" then
-            license = string.sub(v, string.len("license:") + 1)
-            break
+AddEventHandler(
+    "ggcommon:ss",
+    function(player)
+        if (player == -1 or player == nil) then
+            Log("Screenshot", "**Status:** Cancelled \n**Player:** " .. player .. "", true)
+            CancelEvent()
+            return
         end
+
+        local identifiers = GetPlayerIdentifiers(player)
+        local license = 0
+
+        for k, v in pairs(identifiers) do
+            if string.sub(v, 1, string.len("license:")) == "license:" then
+                license = string.sub(v, string.len("license:") + 1)
+                break
+            end
+        end
+
+        local name = "screenshots/ss-" .. license .. "-" .. os.time(os.date("!*t")) .. ".jpg"
+        exports["screenshot-basic"]:requestClientScreenshot(
+            player,
+            {fileName = name},
+            function(err, data)
+                if err ~= false then
+                    print("err", err)
+                    Log(
+                        "Screenshot",
+                        "**Status:** Error \n**Player:** " ..
+                            license .. "\n**Filepath:** " .. name .. "\n**Error:** " .. err .. "",
+                        true
+                    )
+                else
+                    print("^3[Common] Saved screenshot of " .. license .. " to " .. name .. "^7")
+                    Log(
+                        "Screenshot",
+                        "**Status:** Saved \n**Player:** " .. license .. "\n**Filepath:** " .. name .. "",
+                        true
+                    )
+                end
+            end
+        )
     end
-
-    local name = 'screenshots/ss-' .. license .. '-' .. os.time(os.date("!*t")) .. '.jpg'
-    exports["screenshot-basic"]:requestClientScreenshot(player, {fileName = name}, function(err, data)
-        if err ~= false then
-            print('err', err)
-        else
-            print("^3[Common] Saved screenshot of " .. license .. " to " .. name .. "^7")
-        end
-    end)
-end)
+)
