7a01ff7de7a5b683404411820c66968c9a92df10
d0p3t <d0p3t@users.noreply.github.com>
2020-01-08 17:12:09 +0100

    Format sv_sentry.lua

--- sv_sentry.lua
+++ sv_sentry.lua
@@ -3,13 +3,13 @@
 --
 ---
 
-local hextable = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'}
+local hextable = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"}
 
 local function RandomHex(length)
   local s = ""
   for i = 1, length do
-    math.randomseed(os.clock()^5)
-    s = s..hextable[math.random(1,16)]
+    math.randomseed(os.clock() ^ 5)
+    s = s .. hextable[math.random(1, 16)]
   end
   return s
 end
@@ -19,27 +19,29 @@ local function GenerateUUID()
 end
 
 local function ISODate(time)
-	return os.date("!%Y-%m-%dT%H:%M:%S", time);
+  return os.date("!%Y-%m-%dT%H:%M:%S", time)
 end
 
 --
 -- Setup Sentry
 --
 ---
-Citizen.CreateThread(function()
-  Config.SentryPub = GetConvar("sentry_pub", "0")
-  Config.SentryPriv = GetConvar("sentry_priv", "0")
-  Config.SentryId = GetConvar("sentry_id", "0")
-  Config.SentryEndpoint = GetConvar("sentry_ip", "127.0.0.1:30120")
-  Config.SentryVersion = GetConvar("sentry_version", "v0000")
-  Config.SentryOS = GetConvar("sentry_os", "Windows")
+Citizen.CreateThread(
+  function()
+    Config.SentryPub = GetConvar("sentry_pub", "0")
+    Config.SentryPriv = GetConvar("sentry_priv", "0")
+    Config.SentryId = GetConvar("sentry_id", "0")
+    Config.SentryEndpoint = GetConvar("sentry_ip", "127.0.0.1:30120")
+    Config.SentryVersion = GetConvar("sentry_version", "v0000")
+    Config.SentryOS = GetConvar("sentry_os", "Windows")
 
-  if(Config.SentryPub == "0" or Config.SentryPriv == "0" or Config.SentryId == "0") then
-    PrintLog("[Common] Sentry not configured correctly. Please check ConVars.")
-  else
-    Config.SentryEnabled = true
+    if (Config.SentryPub == "0" or Config.SentryPriv == "0" or Config.SentryId == "0") then
+      PrintLog("[Common] Sentry not configured correctly. Please check ConVars.")
+    else
+      Config.SentryEnabled = true
+    end
   end
-end)
+)
 
 --
 --  Send Sentry issue
@@ -52,7 +54,9 @@ end)
 -- @param tags Table of tags
 -- @param source Player source if applicable
 function SentryIssue(errorType, error, level, tags, source)
-  if not Config.SentryEnabled then return end
+  if not Config.SentryEnabled then
+    return
+  end
 
   local data = {
     ["event_id"] = GenerateUUID(),
@@ -73,34 +77,41 @@ function SentryIssue(errorType, error, level, tags, source)
     ["server_name"] = Config.SentryEndpoint
   }
 
-
   local headers = {
-    ["Content-Type"] = 'application/json',
-    ["User-Agent"] = 'ggcommon/1.0',
+    ["Content-Type"] = "application/json",
+    ["User-Agent"] = "ggcommon/1.0",
     ["X-Sentry-Auth"] = sentryAuthHeader()
   }
 
-  PerformHttpRequest("https://sentry.io/api/" .. Config.SentryId .. "/store/", function(statusCode, data, headers)
-    if statusCode ~= 200 then
-      PrintLog("" .. statusCode .. "Error occurred when sending Issue to Sentry -> " .. data .. "", "error")
-    end
-  end, "POST", json.encode(data), headers)
+  PerformHttpRequest(
+    "https://sentry.io/api/" .. Config.SentryId .. "/store/",
+    function(statusCode, data, headers)
+      if statusCode ~= 200 then
+        PrintLog("" .. statusCode .. "Error occurred when sending Issue to Sentry -> " .. data .. "", "error")
+      end
+    end,
+    "POST",
+    json.encode(data),
+    headers
+  )
 end
 
 local function getContexts()
-	return {
-		os = {
-			name = Config.SentryOS,
-		},
-		runtime = {
-			name = "FXServer",
-			version = Config.SentryVersion,
-		}
-	}
+  return {
+    os = {
+      name = Config.SentryOS
+    },
+    runtime = {
+      name = "FXServer",
+      version = Config.SentryVersion
+    }
+  }
 end
 
 local function getUserContext(source)
-  if source == 0 or source == nil then return nil end
+  if source == 0 or source == nil then
+    return nil
+  end
 
   local name = GetPlayerName(source)
   local endpoint = GetPlayerEndpoint(source)
@@ -122,7 +133,9 @@ local function getUserContext(source)
 end
 
 local function sentryAuthHeader()
- return "Sentry sentry_version=7,sentry_timestamp="..os.time()..",sentry_key="..Config.SentryPub..",sentry_secret="..Config.SentryPriv..",sentry_client=ggcommon/1.0"
+  return "Sentry sentry_version=7,sentry_timestamp=" ..
+    os.time() ..
+      ",sentry_key=" .. Config.SentryPub .. ",sentry_secret=" .. Config.SentryPriv .. ",sentry_client=ggcommon/1.0"
 end
 
 local function getSDKData()
