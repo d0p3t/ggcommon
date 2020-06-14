fcbefaa8ac9101bab8efb9941532cc1d80375c59
d0p3t <d0p3t@users.noreply.github.com>
2020-06-14 16:56:27 +0200

    experimental: new map hud

---
+++ client/cl_hud.lua
@@ -0,0 +1,34 @@
+Citizen.CreateThread(
+  function()
+    while not NetworkIsPlayerActive(PlayerId()) do
+      Wait(0)
+    end
+
+    RequestStreamedTextureDict("circlemap", false)
+    while not HasStreamedTextureDictLoaded("circlemap") do
+      Wait(0)
+    end
+
+    local minimap = RequestScaleformMovie("minimap")
+    while not HasScaleformMovieLoaded(minimap) do
+      Wait(0)
+    end
+
+    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
+    SetMinimapClipType(1)
+    SetMinimapComponentPosition("minimap", "L", "B", -0.0045, -0.022, 0.210, 0.258888)
+    SetMinimapComponentPosition("minimap_mask", "L", "B", 0.0, 0.032, 0.101, 0.259)
+    SetMinimapComponentPosition("minimap_blur", "L", "B", 0.012, 0.022, 0.256, 0.337)
+
+    SetRadarBigmapEnabled(true, false)
+    Wait(500)
+    SetRadarBigmapEnabled(false, false)
+
+    while true do
+      Wait(0)
+      BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+      ScaleformMovieMethodAddParamInt(3)
+      EndScaleformMovieMethod()
+    end
+  end
+)
