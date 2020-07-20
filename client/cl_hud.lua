eb7001497d1c1e5650a18bb2a5369738443f1da3
d0p3t <d0p3t@users.noreply.github.com>
2020-07-20 20:56:01 +0200

    Add prompt event for shop

--- client/cl_hud.lua
+++ client/cl_hud.lua
@@ -1,35 +1,18 @@
-Citizen.CreateThread(
-  function()
-    while not NetworkIsPlayerActive(PlayerId()) do
-      Wait(0)
-    end
-
-    RequestStreamedTextureDict("circlemap", false)
-    while not HasStreamedTextureDictLoaded("circlemap") do
-      Wait(0)
-    end
-
-    local minimap = RequestScaleformMovie("minimap")
-    while not HasScaleformMovieLoaded(minimap) do
-      Wait(0)
-    end
-
-    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
-    SetMinimapClipType(1)
-    SetMinimapComponentPosition("minimap", "L", "B", -0.0045, -0.022, 0.210, 0.258888)
-    SetMinimapComponentPosition("minimap_mask", "L", "B", 0.0, 0.032, 0.101, 0.259)
-    SetMinimapComponentPosition("minimap_blur", "L", "B", 0.012, 0.022, 0.256, 0.337)
-
-    SetRadarBigmapEnabled(true, false)
-    Wait(500)
-    --SetRadarBigmapEnabled(false, false)
-
-    while true do
-      Wait(0)
-      SetRadarBigmapEnabled(false, false)
-      BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
-      ScaleformMovieMethodAddParamInt(3)
-      EndScaleformMovieMethod()
+RegisterNetEvent("shop:prompt")
+AddEventHandler(
+  "shop:prompt",
+  function(result)
+    if result ~= "" then
+      SendNuiMessage(
+        json.encode(
+          {
+            type = "ggtoaster",
+            toasterMessage = result,
+            toasterTop = true,
+            toasterPosition = "center"
+          }
+        )
+      )
     end
   end
 )
