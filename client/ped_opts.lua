05aa64a5b01cefdf7a2a66054a1bc9abecdb25d3
d0p3t <d0p3t@users.noreply.github.com>
2020-10-10 21:36:22 +0200

    new: Disable pistol whipping

--- client/ped_opts.lua
+++ client/ped_opts.lua
@@ -33,17 +33,18 @@ Citizen.CreateThread(
         SetVehiclePopulationBudget(0.0)
         while true do
             Wait(0)
-            --SetPedDensityMultiplierThisFrame(0.0)
-            --SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
-            --SetVehicleDensityMultiplierThisFrame(0.0)
-            --SetParkedVehicleDensityMultiplierThisFrame(0.0)
-            --SetRandomVehicleDensityMultiplierThisFrame(0.0)
             SetDeepOceanScaler(0.0)
             N_0xc54a08c85ae4d410(1.0)
             HideHudComponentThisFrame(6)
             HideHudComponentThisFrame(7)
             HideHudComponentThisFrame(8)
             HideHudComponentThisFrame(9)
+            
+            if IsPedArmed(PlayerPedId(),6) then
+                DisableControlAction(1, 140, true)
+            	DisableControlAction(1, 141, true)
+            	DisableControlAction(1, 142, true)
+            end
         end
     end
 )
