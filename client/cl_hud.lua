1a15ace3351e03f46d5b4b622ebcac0eaf4f8a47
d0p3t <d0p3t@users.noreply.github.com>
2020-10-10 21:35:46 +0200

    new: Show current map's name

--- client/cl_hud.lua
+++ client/cl_hud.lua
@@ -16,10 +16,37 @@
 --     end
 --   end
 -- )
+local currentMapName = ""
+
+RegisterNetEvent("onRoundEnded")
+AddEventHandler("onRoundEnded", function()
+  currentMapName = ""
+end)
+
+RegisterNetEvent("mapName")
+AddEventHandler("mapName", function(name)
+  if name ~= nil then
+    currentMapName = name
+  else
+    currentMapName = ""
+  end
+end)
 
 Citizen.CreateThread(
   function()
     Wait(100)
     AddTextEntry("FE_THDR_GTAO", "Gun Game V")
+    AddTextEntry("MAP_NAME_LABEL", "Map: ~a~")
+    while true do
+      Wait(0)
+      if currentMapName ~= "" then
+        SetTextFont(4)
+        SetTextScale(0.3,0.3)
+        SetTextColour(255,255,255,180)
+        BeginTextCommandDisplayText("MAP_NAME_LABEL")
+        AddTextComponentSubstringPlayerName(currentMapName)
+        EndTextCommandDisplayText(0.015,0.981)
+      end
+    end
   end
 )
