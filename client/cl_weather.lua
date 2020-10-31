9c89cf1c35c052e9457f846dc9e8bbaad377cecc
d0p3t <d0p3t@users.noreply.github.com>
2020-10-31 14:04:08 +0100

    Halloween weather

--- client/cl_weather.lua
+++ client/cl_weather.lua
@@ -3,10 +3,26 @@ AddEventHandler(
   "setTimeWeather",
   function(h, m, s, w)
     ClearWeatherTypePersist()
-    SetWeatherTypeNow(w)
-    SetWeatherTypeOvertimePersist(w, 1)
     PauseClock(false)
-    NetworkOverrideClockTime(h, m, s)
+    local chance = math.random(0, 100)
+    if chance <= 50.0 then
+      SetWeatherTypeNow(w)
+      SetWeatherTypeOvertimePersist(w, 1)
+      NetworkOverrideClockTime(h, m, s)
+    else
+      SetWeatherTypeNow("HALLOWEEN")
+      SetWeatherTypeOvertimePersist("HALLOWEEN", 1)
+      TriggerEvent(
+        "chat:addMessage",
+        {
+          templateId = "help",
+          color = {255, 255, 255},
+          multiline = true,
+          args = {"Halloween weather! 🎃 Oh no, spooky...", ""}
+        }
+      )
+      NetworkOverrideClockTime(0, 0, 0)
+    end
     PauseClock(true)
   end
 )
