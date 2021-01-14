7a14ee59298d52ef479b9f8fe850381665f6f1db
d0p3t <d0p3t@users.noreply.github.com>
2021-01-14 14:02:27 +0100

    Update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version "adamant"
+fx_version "cerulean"
 game "gta5"
 
 name "GGCommon"
@@ -9,23 +9,23 @@ url "https://github.com/d0p3t/ggcommon"
 dependency "screenshot-basic"
 
 client_scripts {
-    "client/*.lua"
+  "client/*.lua"
 }
 
 server_scripts {
-    "server/*.lua"
+  "server/*.lua"
 }
 
 files {
-	'client/lib/Newtonsoft.Json.dll',
-	'client/lib/index.html',
-    'client/lib/audio/**/*.ogg',
-    'client/lib/*.min.css',
-    'client/lib/*.css',
-    'client/lib/*.min.js'
+  "client/lib/Newtonsoft.Json.dll",
+  "client/lib/index.html",
+  "client/lib/audio/**/*.ogg",
+  "client/lib/*.min.css",
+  "client/lib/*.css",
+  "client/lib/*.min.js"
 }
 
-ui_page 'client/lib/index.html'
+ui_page "client/lib/index.html"
 
 server_export "PrintLog"
 server_export "Log"
