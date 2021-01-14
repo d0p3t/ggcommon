dc0ed10c3d0c2892618f29a721cbc868a53d75a9
d0p3t <d0p3t@users.noreply.github.com>
2021-01-14 13:50:46 +0100

    Possible fix for players without blips

--- client/cl_playerBlips.lua
+++ client/cl_playerBlips.lua
@@ -1,3 +1,24 @@
+local createdPlayerBlips = {}
+
+local function createBlip(player)
+  if player == Cache.ClientPlayerId then
+    return
+  end
+
+  local ped = GetPlayerPed(player)
+  local blip = GetBlipFromEntity(ped)
+
+  if blip == 0 then
+    blip = AddBlipForEntity(ped)
+    SetBlipScale(blip, 0.86)
+    ShowHeadingIndicatorOnBlip(blip, true)
+    SetBlipCategory(blip, 7)
+    SetBlipSprite(blip, 1)
+    SetBlipColour(blip, 6)
+    createdPlayerBlips[player] = blip
+  end
+end
+
 AddEventHandler(
   "gameEventTriggered",
   function(name, args)
@@ -37,29 +58,31 @@ AddEventHandler(
     if blip ~= 0 then
       SetBlipSprite(blip, 1)
       SetBlipColour(blip, 6)
+    else
+      createBlip(player)
     end
   end
 )
 
-RegisterNetEvent("onPlayerJoining")
-AddEventHandler(
-  "onPlayerJoining",
-  function(netId)
-    local player = GetPlayerFromServerId(netId)
-    if player == Cache.ClientPlayerId then
-      return
-    end
+Citizen.CreateThread(
+  function()
+    while true do
+      Wait(1000)
+      for _, player in ipairs(GetActivePlayers()) do
+        if not createdPlayerBlips[player] then
+          createBlip(player)
+        end
+      end
 
-    local ped = GetPlayerPed(player)
-    local blip = GetBlipFromEntity(ped)
-
-    if blip == 0 then
-      blip = AddBlipForEntity(ped)
-      SetBlipScale(blip, 0.86)
-      ShowHeadingIndicatorOnBlip(blip, true)
-      SetBlipCategory(blip, 7)
-      SetBlipSprite(blip, 1)
-      SetBlipColour(blip, 6)
+      for player, blip in pairs(createdPlayerBlips) do
+        -- check if player is still active
+        if not NetworkIsPlayerActive(player) or GetPlayerPed(player) == 0 then
+          if DoesBlipExist(blip) then
+            RemoveBlip(blip)
+          end
+          createdPlayerBlips[player] = nil
+        end
+      end
     end
   end
 )
