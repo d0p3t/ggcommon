9de3fed27a2e1dbf169bd8847c356f76f525ccb3
d0p3t <d0p3t@users.noreply.github.com>
2021-04-28 13:15:45 +0200

    Add key for force respawn (F1)

--- client/cl_cmds.lua
+++ client/cl_cmds.lua
@@ -5,11 +5,13 @@ local set_entity_invincible = SetEntityInvincible
 local set_entity_health = SetEntityHealth
 
 function Respawn()
-  local ped = Cache.ClientPedId
-  set_entity_invincible(ped, false)
-  set_entity_health(ped, 0)
+    local ped = Cache.ClientPedId
+    set_entity_invincible(ped, false)
+    set_entity_health(ped, 0)
 end
 
 RegisterCommand("suicide", Respawn, false)
 RegisterCommand("die", Respawn, false)
 RegisterCommand("respawn", Respawn, false)
+
+RegisterKeyMapping("suicide", "Force Respawn", "keyboard", "f1")
