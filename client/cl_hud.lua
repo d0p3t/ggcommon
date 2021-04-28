626d7331fead3e82cbd6ad795ac4f7b8d1ad3e86
d0p3t <d0p3t@users.noreply.github.com>
2021-04-28 13:17:32 +0200

    Don't run current map thread every frame if no map name is set

--- client/cl_hud.lua
+++ client/cl_hud.lua
@@ -30,13 +30,17 @@ Citizen.CreateThread(
     Wait(100)
     AddTextEntry("FE_THDR_GTAO", "Gun Game V")
     AddTextEntry("MAP_NAME_LABEL", "Map: ~a~")
+    local sleep = 500
     while true do
-      Wait(0)
+      Wait(sleep)
       if currentMapName ~= "" then
+        sleep = 0
         set_text_scale(0.2, 0.2)
         begin_text_command_display_text("MAP_NAME_LABEL")
         add_text_component_substring_player_name(currentMapName)
         end_text_command_display_text(0.015, 0.981)
+      else
+        sleep = 500
       end
     end
   end
